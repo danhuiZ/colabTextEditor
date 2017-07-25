a9d09d542ce28d096c3c257474a31581d5538518
mohammad-gitup <mohammadsyed.kc@gmail.com>
2017-07-25 12:58:43 -0700

    Updated login flow to have axios

--- reactApp/components/registration.js
+++ reactApp/components/registration.js
@@ -1,21 +1,47 @@
-var React = require('React');
-
+import React from 'react';
+import axios from 'axios';
 
 // or change it to axios request
 
 class Registration extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state  = {
+      username: '',
+      password: ''
+    };
+    this.handleUser = this.handleUser.bind(this);
+    this.handlePass = this.handlePass.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleUser(e) {
+    this.setState({username: e.target.value});
+  }
+
+  handlePass(e) {
+    this.setState({password: e.target.value});
+  }
+
+  handleSubmit() {
+    axios.post('http://localhost:3000/register', {
+      username: this.state.username,
+      password: this.state.password
+    })
+    .then(function(resp) {
+      console.log('We got a response!', resp);
+    });
+  }
+
   render() {
     return(
       <div>
-      <h1>Registrater!</h1>
-      <form method="POST" action="http://localhost:3000/register">
-        <input name="username" placeholder="Enter a username..." id="username"></input>
+      <h1>Register!</h1>
+        <input name="username" placeholder="Enter a username..." value={this.state.username} onChange={(e) => this.handleUser(e)} id="username"></input>
         <br></br>
-        <input name="password" placeholder="Enter a password..." id="password"></input>
+        <input name="password" placeholder="Enter a password..." value={this.state.password} onChange={(e) => this.handlePass(e)} id="password"></input>
         <br></br>
-        <input type="submit" value="Submit"></input>
-      </form>
-
+        <button onClick={() => this.handleSubmit()}></button>
       </div>
     );
   }
