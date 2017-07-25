f0fc1c5dde1876176e439c9496853067093b759d
mohammad-gitup <mohammadsyed.kc@gmail.com>
2017-07-25 11:31:37 -0700

    Added login and registration features

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,88 @@
 const express = require('express');
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
+var bodyParser = require('body-parser');
 const app = express();
+
+var User = null;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// PASSPORT FLOW
+
+passport.serializeUser(function(user, done) {
+  done(null, user._id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new LocalStrategy(function(username, password, done) {
+  console.log('this is the user name', username);
+  // Find the user with the given username
+  User.findOne({ username: username }, function (err, user) {
+    // if there's an error, finish trying to authenticate (auth failed)
+    if (err) {
+      console.log(err);
+      return done(err);
+    }
+    // if no user present, auth failed
+    if (!user) {
+      console.log('shit boy');
+      return done(null, false, { message: 'Incorrect username.' });
+    }
+    // if passwords do not match, auth failed
+    if (user.password !== password) {
+      return done(null, false, { message: 'Incorrect password.' });
+    }
+    // auth has has succeeded
+    return done(null, user);
+  });
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// app.use('/', passport);
+
+// PASSPORT FLOW
 
 // Example route
 app.get('/', function (req, res) {
-  res.send('hello world');
+  res.send('i got here because i failed');
 });
 
-app.post('/login', function(req, res) {
-  console.log('heres the login flow');
+// need to fix redirects as well
+app.post('/login', passport.authenticate('local', { failureRedirect: '/' }),function(req, res) {
+  console.log(req.body);
+  res.send(200);
+});
+
+// need to fix this
+app.post('/register', function(req, res) {
+  var newUser = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+  newUser.save(function(err, user) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(user);
+    res.redirect('/login');
+  });
+  res.sendStatus(200);
+});
+
+// need to fix this
+app.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/login');
 });
 
 app.listen(3000, function () {
--- reactApp/routes.js
+++ reactApp/routes.js
@@ -1,11 +1,13 @@
 var React = require('React');
 import Document from './components/document.js';
 import Login from './components/login.js';
+import Registration from './components/registration.js';
 import { Switch, Route } from 'react-router-dom';
 
 const Routes = () => (
   <Switch>
     <Route exact path ="/" component={Document} />
+    <Route exact path="/registration" component={Registration} />
     <Route path="/login" component={Login} />
   </Switch>
 );
