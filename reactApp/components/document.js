2ebd26b49a2f576d1f82c8b4b7b699db656aea1c
Andrew Eells <andrew.eells3@gmail.com>
2017-07-26 16:28:50 -0700

    redesigned doc ui

--- reactApp/components/document.js
+++ reactApp/components/document.js
@@ -26,6 +26,8 @@ const myBlockTypes = DefaultDraftBlockRenderMap.merge(new Map({
 
 class Document extends React.Component {
 
+  //TODO:  , numbers, bullet, left, center, right, increase font, decrease font
+
   constructor(props) {
     super(props);
     this.state = {
@@ -33,6 +35,9 @@ class Document extends React.Component {
       editorState: EditorState.createEmpty(),
       inlineStyles: {},
       fontSize: 12,
+      openColorPicker: false,
+      openHighlighter: false,
+      colorPickerButton: null
     };
     this.onChange = (editorState) => {
       this.setState({editorState});
@@ -58,8 +63,44 @@ class Document extends React.Component {
     });
   }
 
-  formatColor(color) {
-    console.log('COLOR IS', color);
+// TEXT COLOR PICKER
+  // renders a color picker button
+  colorPicker() {
+    return (
+      <div style={{display: 'inline-block'}}>
+        <FlatButton
+          backgroundColor={colors.blue200}
+          icon={<FontIcon className='material-icons'>format_color_fill</FontIcon>}
+          onClick={this.openColorPicker.bind(this)}
+        />
+        <Popover
+          open={this.state.openColorPicker}
+          anchorEl={this.state.colorPickerButton}
+          anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
+          targetOrigin={{horizontal: 'left', vertical: 'top'}}
+          onRequestClose={this.closeColorPicker.bind(this)}
+        >
+          <TwitterPicker onChangeComplete={this.textColor.bind(this)}/>
+        </Popover>
+      </div>
+    );
+  }
+
+  openColorPicker(e) {
+    this.setState({
+      openColorPicker: true,
+      colorPickerButton: e.target
+    });
+  }
+
+  closeColorPicker() {
+    this.setState({
+      openColorPicker: false,
+    });
+  }
+
+  textColor(color) {
+    console.log('BOLOR IS', color);
     var newInlineStyles = Object.assign({}, this.state.inlineStyles,
       {[color.hex]: {
         color: color.hex,
@@ -67,11 +108,48 @@ class Document extends React.Component {
     );
     this.setState({
       inlineStyles: newInlineStyles,
-      editorState: RichUtils.toggleInlineStyle(this.state.editorState, color.hex)
+      editorState: RichUtils.toggleInlineStyle(this.state.editorState, color.hex),
+      openColorPicker: false
     });
   }
 
-  highlightText(color) {
+// HIGHLIGHT COLOR PICKER
+  // renders a highlight picker button
+  highlighter() {
+    return (
+      <div style={{display: 'inline-block'}}>
+        <FlatButton
+          backgroundColor={colors.blue200}
+          icon={<FontIcon className='material-icons'>highlight</FontIcon>}
+          onClick={this.openHighlighter.bind(this)}
+        />
+        <Popover
+          open={this.state.openHighlighter}
+          anchorEl={this.state.highlighterButton}
+          anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
+          targetOrigin={{horizontal: 'left', vertical: 'top'}}
+          onRequestClose={this.closeHighlighter.bind(this)}
+        >
+          <TwitterPicker onChangeComplete={this.highlightColor.bind(this)}/>
+        </Popover>
+      </div>
+    );
+  }
+
+  openHighlighter(e) {
+    this.setState({
+      openHighlighter: true,
+      highlighterButton: e.target
+    });
+  }
+
+  closeHighlighter() {
+    this.setState({
+      openHighlighter: false,
+    });
+  }
+
+  highlightColor(color) {
     var newInlineStyles = Object.assign({}, this.state.inlineStyles,
       {['highlight' + color.hex]: {
         backgroundColor: color.hex,
@@ -79,10 +157,13 @@ class Document extends React.Component {
     );
     this.setState({
       inlineStyles: newInlineStyles,
-      editorState: RichUtils.toggleInlineStyle(this.state.editorState, String('highlight' + color.hex))
+      editorState: RichUtils.toggleInlineStyle(this.state.editorState, String('highlight' + color.hex)),
+      openHighlighter: false
     });
     console.log("INLINESTYLES", this.state.inlineStyles);
   }
+
+// NEXT THING
 
   toggleFormat(e, style, block) {
     e.preventDefault();      //prevent the editor from losing focus, can also use ref
@@ -117,69 +198,6 @@ class Document extends React.Component {
     );
   }
 
-  openColorPicker(e) {
-    this.setState({
-      openColorPicker: true,
-      colorPickerButton: e.target
-    });
-  }
-  closeColorPicker() {
-    this.setState({
-      openColorPicker: false,
-    });
-  }
-  colorPicker() {
-    return (
-      <div style={{display: 'inline-block'}}>
-        <FlatButton
-          backgroundColor={colors.blue200}
-          icon={<FontIcon className='material-icons'>format_color_fill</FontIcon>}
-          onClick={this.openColorPicker.bind(this)}
-        />
-        <Popover
-          open={this.state.openColorPicker}
-          anchorEl={this.state.colorPickerButton}
-          anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
-          targetOrigin={{horizontal: 'left', vertical: 'top'}}
-          onRequestClose={this.closeColorPicker.bind(this)}
-        >
-          <TwitterPicker onChangeComplete={this.formatColor.bind(this)}/>
-        </Popover>
-      </div>
-    );
-  }
-
-  openHighlighter(e) {
-    this.setState({
-      openHighlighter: true,
-      highlighterButton: e.target
-    });
-  }
-  closeHighlighter() {
-    this.setState({
-      openHighlighter: false,
-    });
-  }
-  highlighter() {
-    return (
-      <div style={{display: 'inline-block'}}>
-        <FlatButton
-          backgroundColor={colors.blue200}
-          icon={<FontIcon className='material-icons'>highlight</FontIcon>}
-          onClick={this.openHighlighter.bind(this)}
-        />
-        <Popover
-          open={this.state.openHighlighter}
-          anchorEl={this.state.highlighterButton}
-          anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
-          targetOrigin={{horizontal: 'left', vertical: 'top'}}
-          onRequestClose={this.closeHighlighter.bind(this)}
-        >
-          <TwitterPicker onChangeComplete={this.highlightText.bind(this)}/>
-        </Popover>
-      </div>
-    );
-  }
 
   applyIncreaseFontSize(shrink) {
     var newFontSize = this.state.fontSize + (shrink ? -4 : 4);
@@ -220,65 +238,110 @@ class Document extends React.Component {
 
   render() {
     return (
-    <div>
-        <h1>{this.state.title}</h1>
-        <div className="navigation">
-          <a className="docID">{`Document ID: ${this.props.match.params.docID}`}</a>
-          <Link to='/doc-portal'>
+      <div className="WRAPPER">
+
+          <div>
+            <h1>{this.state.title}</h1>
+            <a className="docID">{`Document ID: ${this.props.match.params.docID}`}</a>
+          </div>
+
+          <div className="navigation">
+
+            <div className="back-docportal">
+              <Link to='/doc-portal'>
+                <FlatButton
+                  className="button"
+                  label="Back to Documents Portal"
+                  icon={<FontIcon className='material-icons'>navigate_before</FontIcon>}
+                />
+              </Link>
+            </div>
+
+            <div className="save">
+              <FlatButton
+                className="button"
+                label="Save Changes"
+                icon={<FontIcon className='material-icons'>save</FontIcon>}
+                onTouchTap={this._onSaveClick.bind(this)}
+              />
+            </div>
+
+          </div>
+
+          <div className="toolbar1">
+            <div className="toolbar-item">
+              {this.formatButton({icon: 'format_bold', style: 'BOLD'})}
+            </div>
+            <div className="toolbar-item">
+              {this.formatButton({icon: 'format_italic', style: 'ITALIC'})}
+            </div>
+            <div className="toolbar-item">
+              {this.formatButton({icon: 'format_underlined', style: 'UNDERLINE'})}
+            </div>
+            <div className="toolbar-item">
+              {this.formatButton({icon: 'format_strikethrough', style: 'STRIKETHROUGH'})}
+            </div>
+          </div>
+
+          <div className="toolbar2">
+            <div className="toolbar-item">
+              {this.formatButton({icon: 'format_align_left', style: 'unstyled', block: true })}
+            </div>
+            <div className="toolbar-item">
+              {this.formatButton({icon: 'format_align_center', style: 'center', block: true })}
+            </div>
+            <div className="toolbar-item">
+              {this.formatButton({icon: 'format_align_right', style: 'right', block: true })}
+            </div>
+            <div className="toolbar-item">
+              {this.formatButton({icon: 'format_list_numbered', style: 'ordered-list-item', block: true })}
+            </div>
+            <div className="toolbar-item">
+              {this.formatButton({icon: 'format_list_bulleted', style: 'unordered-list-item', block: true })}
+            </div>
+          </div>
+
+          <div className="toolbar3">
+            <div className="toolbar-item">
+              {this.colorPicker()}
+            </div>
+            <div className="toolbar-item">
+              {this.increaseFontSize(false)}
+            </div>
+            <div className="toolbar-item">
+              {this.increaseFontSize(true)}
+            </div>
+            <div className="toolbar-item">
+              {this.highlighter()}
+            </div>
+          </div>
+
+          <div className="container">
+            <Editor
+              ref="editor"
+              blockRenderMap={myBlockTypes}
+              customStyleMap={this.state.inlineStyles}
+              editorState={this.state.editorState}
+              onChange={this.onChange}
+              spellCheck={true}
+            />
+        </div>
+        <div className="registration">
+          <Link to='/'>
             <FlatButton
               className="button"
-              label="Back to Documents Portal"
-              icon={<FontIcon className='material-icons'>navigate_before</FontIcon>}
+              label="To Login"
+              icon={<FontIcon className='material-icons'>face</FontIcon>}
             />
           </Link>
-          <FlatButton
-            className="button"
-            label="Save Changes"
-            icon={<FontIcon className='material-icons'>save</FontIcon>}
-            onTouchTap={this._onSaveClick.bind(this)}
-          />
+          <Link to='/registration'>
+            <FlatButton
+              className="button"
+              label="To Registration"
+              icon={<FontIcon className='material-icons'>account_circle</FontIcon>}/>
+          </Link>
         </div>
-        <div className="toolbar">
-          {this.colorPicker()}
-          {this.formatButton({icon: 'format_bold', style: 'BOLD'})}
-          {this.formatButton({icon: 'format_italic', style: 'ITALIC'})}
-          {this.formatButton({icon: 'format_underlined', style: 'UNDERLINE'})}
-          {this.formatButton({icon: 'format_strikethrough', style: 'STRIKETHROUGH'})}
-          {this.highlighter()}
-          {this.formatButton({icon: 'format_list_numbered', style: 'ordered-list-item', block: true })}
-          {this.formatButton({icon: 'format_list_bulleted', style: 'unordered-list-item', block: true })}
-          {this.formatButton({icon: 'format_align_left', style: 'unstyled', block: true })}
-          {this.formatButton({icon: 'format_align_center', style: 'center', block: true })}
-          {this.formatButton({icon: 'format_align_right', style: 'right', block: true })}
-          {this.increaseFontSize(false)}
-          {this.increaseFontSize(true)}
-        </div>
-        <div className="container">
-          <Editor
-            ref="editor"
-            blockRenderMap={myBlockTypes}
-            customStyleMap={this.state.inlineStyles}
-            editorState={this.state.editorState}
-            onChange={this.onChange}
-            spellCheck={true}
-          />
       </div>
-      <div className="navigation">
-        <Link to='/'>
-          <FlatButton
-            className="button"
-            label="To Login"
-            icon={<FontIcon className='material-icons'>face</FontIcon>}
-          />
-        </Link>
-        <Link to='/registration'>
-          <FlatButton
-            className="button"
-            label="To Registration"
-            icon={<FontIcon className='material-icons'>account_circle</FontIcon>}/>
-        </Link>
-      </div>
-    </div>
     );
   }
 }
