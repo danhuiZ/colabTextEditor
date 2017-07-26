81221f5db406aceabf48008dfd098233132ed29a
danhuiZ <danhuizzz@gmail.com>
2017-07-25 17:29:37 -0700

    again

--- reactApp/routes.js
+++ reactApp/routes.js
@@ -1,16 +1,14 @@
-var React = require('react');
+var React = require('React');
 import Document from './components/document.js';
 import Login from './components/login.js';
 import Registration from './components/registration.js';
-import DocPortal from './components/docportal.js';
 import { Switch, Route } from 'react-router-dom';
 
 const Routes = () => (
   <Switch>
-    <Route exact path ="/" component={Login} />
+    <Route exact path ="/" component={Document} />
     <Route exact path="/registration" component={Registration} />
-    <Route exact path="/documents" component={Document} />
-    <Route exact path="/doc-portal" component={DocPortal} />
+    <Route path="/login" component={Login} />
   </Switch>
 );
 
