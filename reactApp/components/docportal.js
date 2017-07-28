4308af1694b9b14b1b2c57e0b4b76e0238f8600c
mohammad-gitup <mohammadsyed.kc@gmail.com>
2017-07-27 18:15:43 -0700

    testing where this is

--- reactApp/components/docportal.js
+++ reactApp/components/docportal.js
@@ -6,6 +6,8 @@ import * as colors from 'material-ui/styles/colors';
 import { List, ListItem } from 'material-ui/List';
 import { EditorState, convertToRaw } from 'draft-js';
 
+console.log('sup');
+
 import Subheader from 'material-ui/Subheader';
 import Avatar from 'material-ui/Avatar';
 import ActionAssignment from 'material-ui/svg-icons/action/assignment';
@@ -37,7 +39,7 @@ export default class DocPortal extends React.Component {
     var title = this.state.docID;
     smalltalk.prompt("Create Document", "Please enter a password for " + title + ': ')
     .then(function(password){
- 
+
       axios.post('http://localhost:3000/newdoc', {
         title: title,
         password: password,
